3ff254855ec14e2eea473459d025abb1ef46ef90
simonheglasik <simonheglasik@gmail.com>
2019-11-05 18:44:36 +0100

    04.02

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -5,8 +5,16 @@ namespace Open_Lab_04._02
     public class Doubler
     {
         public string DoubleChar(string original)
-        {
-            throw new NotImplementedException();
+        { 
+            string a="";
+            for(int i = 0;i<original.Length;i++)
+            {
+                for (int v = 0;v<2;v++)
+                {
+                    a = a + original[i];
+                }
+            }
+            return a;
         }
     }
 }
